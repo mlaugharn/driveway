634210c8c4449fb7a4115a3d6e74898f48128669
mlaugharn <mlaugharn@gmail.com>
2013-06-05 13:56:56 -0700

    bugfixes and optimization

--- simulation.py
+++ simulation.py
@@ -1,31 +1,42 @@
 import random
 import math
+import os
 
-def simulate(observation_period = 31556926, max_driveway_length = 10, average_arrival_rate = 90, average_service_rate = 45, average_meal_cost = 3, per_meal_cost = .25):
+def simulate(observation_period = 31556926, max_driveway_length = 10, average_arrival_rate = 90, average_service_rate = 60, average_meal_cost = 3, per_meal_cost = .25):
 	total_cars = 0
 	cars_rejected = 0
 	seconds_waited = 0
 	seconds_passed = 0
 	meals_served = 0
 	profit = 0
-	driveway = []
+	driveway = [0] * max_driveway_length
+	driveway_states = [0] * (max_driveway_length + 1) #0 through incl. 10
+	rand = random.random
+	numcars = 0
 	print("starting simulation...")
 	while seconds_passed < observation_period:
-		if len(driveway) > 0 and driveway[0] <= 0:
+		car_leaves = numcars > 0 and driveway[0] <= 0
+		if car_leaves:
 			driveway.pop(0)
-		car_shows_up = random.random() <= (1 / average_arrival_rate)
+			driveway.append(0)
+			numcars -= 1
+			driveway_states[numcars] += 1
+		car_shows_up = rand() <= (1 / average_arrival_rate)
 		if car_shows_up:
 			total_cars += 1
-			car_enters = len(driveway) < max_driveway_length
+			car_enters = numcars < max_driveway_length
 			if car_enters:
-				driveway.append(average_service_rate)
+				driveway[numcars] = average_service_rate
+				numcars += 1
+				driveway_states[numcars] += 1
 				meals_served += 1
-				profit += average_arrival_rate - per_meal_cost
+				profit += average_meal_cost - per_meal_cost
 			else:
 				cars_rejected += 1
 		for index, car in enumerate(driveway):
-			driveway[index] = car - 1
-			seconds_waited += 1
+			if car > 0:
+				driveway[index] = car - 1
+				seconds_waited += 1
 		seconds_passed += 1
 	print()
 	print("total_cars:", total_cars)
@@ -34,5 +45,5 @@ def simulate(observation_period = 31556926, max_driveway_length = 10, average_ar
 	print("seconds_passed:", seconds_passed)
 	print("meals_served:", meals_served)
 	print("profit:", profit)
-	print("driveway:", driveway)
+	print("driveway_states:", driveway_states)
 	return profit
