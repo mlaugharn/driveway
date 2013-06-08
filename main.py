da4c0950ab8a02790d73cfe6235ae613f89cfe72
mlaugharn <mlaugharn@gmail.com>
2013-06-07 17:54:57 -0700

    updated repo files

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import simulation
 
 if __name__ == '__main__':
-	simulation.simulate(int(input("observation_period (s): \n")),
-int(input("max_driveway_length (cars): \n")),
-int(input("average_arrival_rate (s): \n")),
-int(input("average_service_rate (s): \n")),
-int(input("average_meal_price ($): \n")),
-float(input("per_meal_cost ($): \n")))
+# 	simulation.simulate(int(input("observation_period (s): \n")),
+# int(input("max_driveway_length (cars): \n")),
+# int(input("average_arrival_rate (s): \n")),
+# int(input("average_service_rate (s): \n")),
+# int(input("average_meal_price ($): \n")),
+# float(input("per_meal_cost ($): \n")))
+	simulation.simulate()
